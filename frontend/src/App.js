0176d6a61307862b34d0b053528616198174d082
Liu Xiaoyi <circuitcoder0@gmail.com>
2020-02-13 01:42:53 +0800

    Only show caret for ready users

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
-
 import { BrowserRouter, Route } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import cn from 'classnames';
 
 import Login from './routes/Login';
 
 export default React.memo(() => {
+  const logined = useSelector(store => store.user !== null);
+
   return <div className="container">
     <header>
       <div className="brand"><strong>Jie</strong>Labs</div>
 
-      <div className="stub">
+      <div className={cn("stub", { 'stub-disabled': !logined })}>
         <div className="stub-text">
           Login
         </div>
