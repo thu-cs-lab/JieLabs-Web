13a27dcf1eaea7fa67ac97ccad7d1fc513b91616
Liu Xiaoyi <circuitcoder0@gmail.com>
2020-02-13 00:45:02 +0800

    Command args in create_user

--- backend/src/bin/create_user.rs
+++ backend/src/bin/create_user.rs
@@ -1,18 +1,38 @@
 use backend;
 use dotenv::dotenv;
 use diesel::prelude::*;
+use structopt::StructOpt;
 
-fn main() {
+#[derive(StructOpt)]
+struct Args {
+    #[structopt(short, long)]
+    user_name: String,
+
+    #[structopt(short, long)]
+    password: String,
+
+    #[structopt(short, long)]
+    real_name: Option<String>,
+
+    #[structopt(short, long)]
+    student_id: Option<String>,
+
+    #[structopt(short, long)]
+    class: Option<String>,
+}
+
+#[paw::main]
+fn main(args: Args) {
     dotenv().ok();
     let url = std::env::var("DATABASE_URL").expect("DATABASE_URL");
     let conn = backend::DbConnection::establish(&url).expect("connect");
 
     let new_user = backend::models::NewUser {
-        user_name: String::from("foo"),
-        password: backend::users::hash_password("bar"),
-        real_name: Some(String::from("baz")),
-        student_id: Some(String::from("2020000202")),
-        class: Some(String::from("1"))
+        user_name: args.user_name,
+        password: backend::users::hash_password(&args.password),
+        real_name: args.real_name,
+        student_id: args.student_id,
+        class: args.class,
     };
     diesel::insert_into(backend::schema::users::table)
         .values(&new_user)
