1f532c3ea2ddffaee704398c2c24fe21651c66dc
Jiajie Chen <noc@jiegec.ac.cn>
2020-02-12 20:15:22 +0800

    Add migration and schema for users table

---
+++ backend/migrations/2020-02-12-120630_create_users/up.sql
@@ -0,0 +1,10 @@
+-- Your SQL goes here
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY,
+    user_name TEXT NOT NULL,
+    password TEXT NOT NULL,
+    real_name TEXT,
+    class TEXT,
+    student_id TEXT
+)
+
---
+++ backend/src/schema.rs
@@ -0,0 +1,10 @@
+table! {
+    users (id) {
+        id -> Integer,
+        user_name -> Text,
+        password -> Text,
+        real_name -> Nullable<Text>,
+        class -> Nullable<Text>,
+        student_id -> Nullable<Text>,
+    }
+}
