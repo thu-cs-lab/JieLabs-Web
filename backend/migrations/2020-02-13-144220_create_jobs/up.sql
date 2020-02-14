ee6ef3c295a4c9533b7bd054db2eef02a2658cdf
Jiajie Chen <noc@jiegec.ac.cn>
2020-02-14 16:35:11 +0800

    Use mysql/sqlite compatible sql statements

--- backend/migrations/2020-02-13-144220_create_jobs/up.sql
+++ backend/migrations/2020-02-13-144220_create_jobs/up.sql
@@ -1,9 +1,11 @@
 CREATE TABLE jobs (
-    id INTEGER PRIMARY KEY NOT NULL,
-    submitter TEXT NOT NULL,
-    type TEXT NOT NULL,
-    source TEXT NOT NULL,
-    status TEXT,
-    destination TEXT,
-    task_id TEXT UNIQUE
+    id int NOT NULL,
+    submitter text NOT NULL,
+    type text NOT NULL,
+    source text NOT NULL,
+    status text NULL,
+    destination text NULL,
+    task_id varchar(256) NULL,
+    PRIMARY KEY (id),
+    UNIQUE (task_id)
 )
