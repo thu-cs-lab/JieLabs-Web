d7ffd3f94a50d3c0a2a0ac2d2bb3fbde90d420ef
Jiajie Chen <noc@jiegec.ac.cn>
2020-02-15 16:35:18 +0800

    Implement bitstream handling for mock_board

--- backend/src/bin/mock_board.rs
+++ backend/src/bin/mock_board.rs
@@ -2,38 +2,52 @@ use backend::common::IOSetting;
 use backend::ws_board;
 use env_logger;
 use serde_json;
+use structopt::StructOpt;
 use ws::connect;
 
-fn main() {
+#[derive(StructOpt, Clone)]
+struct Args {
+    #[structopt(short, long, default_value = "127.0.0.1:8080")]
+    host: String,
+}
+
+#[paw::main]
+fn main(args: Args) {
     env_logger::init();
-    connect("ws://127.0.0.1:8080/api/ws_board", |out| {
+    connect(format!("ws://{}/api/ws_board", args.host), |out| {
         out.send(r#"{"Authenticate":{"password":"password","software_version":"1.0","hardware_version":"0.1"}}"#).unwrap();
         move |msg| {
             println!("Client got message '{}'. ", msg);
-            if let ws::Message::Text(text) = msg {
-                if let Ok(msg) = serde_json::from_str::<ws_board::WSBoardMessageS2B>(&text) {
-                    match msg {
-                        ws_board::WSBoardMessageS2B::SetIODirection(direction) => {
-                           println!("Set IO Direction {:?}", direction);
-                           out.send(serde_json::to_string(&ws_board::WSBoardMessageB2S::ReportIOChange(IOSetting {
-                               mask: 0b1111,
-                               data: 0b1111,
-                           })).unwrap()).unwrap();
-                        }
-                        ws_board::WSBoardMessageS2B::SetIOOutput(output) => {
-                           println!("Set IO Output {:?}", output);
-                           out.send(serde_json::to_string(&ws_board::WSBoardMessageB2S::ReportIOChange(IOSetting {
-                               mask: 0b1111,
-                               data: 0b1111,
-                           })).unwrap()).unwrap();
-                        }
-                        ws_board::WSBoardMessageS2B::SubscribeIOChange(_) => {
-                           println!("Subscribe to io change");
-                        }
-                        ws_board::WSBoardMessageS2B::UnsubscribeIOChange(_) => {
-                           println!("Unsubscribe to io change");
+            match msg {
+                ws::Message::Text(text) => {
+                    if let Ok(msg) = serde_json::from_str::<ws_board::WSBoardMessageS2B>(&text) {
+                        match msg {
+                            ws_board::WSBoardMessageS2B::SetIODirection(direction) => {
+                            println!("Set IO Direction {:?}", direction);
+                            out.send(serde_json::to_string(&ws_board::WSBoardMessageB2S::ReportIOChange(IOSetting {
+                                mask: 0b1111,
+                                data: 0b1111,
+                            })).unwrap()).unwrap();
+                            }
+                            ws_board::WSBoardMessageS2B::SetIOOutput(output) => {
+                            println!("Set IO Output {:?}", output);
+                            out.send(serde_json::to_string(&ws_board::WSBoardMessageB2S::ReportIOChange(IOSetting {
+                                mask: 0b1111,
+                                data: 0b1111,
+                            })).unwrap()).unwrap();
+                            }
+                            ws_board::WSBoardMessageS2B::SubscribeIOChange(_) => {
+                            println!("Subscribe to io change");
+                            }
+                            ws_board::WSBoardMessageS2B::UnsubscribeIOChange(_) => {
+                            println!("Unsubscribe to io change");
+                            }
                         }
                     }
+                }
+                ws::Message::Binary(bit) => {
+                    println!("Got bitstream of length {}", bit.len());
+                    out.send(serde_json::to_string(&ws_board::WSBoardMessageB2S::ProgramBitstreamFinish(true)).unwrap()).unwrap();
                 }
             }
             Ok(())
