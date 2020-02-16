df23cc1ad091b76df9ab2ebf26b472a8d3809f3c
Jiajie Chen <noc@jiegec.ac.cn>
2020-02-16 21:31:45 +0800

    Fetch boards and jobs

--- frontend/src/routes/Admin.js
+++ frontend/src/routes/Admin.js
@@ -5,12 +5,18 @@ import { get } from '../util';
 
 export default React.memo(() => {
   const [users, setUsers] = useState([]);
+  const [boards, setBoards] = useState([]);
+  const [jobs, setJobs] = useState([]);
   const history = useHistory();
   const dispatch = useDispatch();
 
   const doRefresh = useCallback(async () => {
     let result = await get('/api/user/list?offset=0&limit=-1');
     setUsers(result.users);
+    result = await get('/api/board/list');
+    setBoards(result);
+    result = await get('/api/task/list?offset=0&limit=-1');
+    setJobs(result.jobs);
   }, []);
 
   return <main className="centering">
@@ -23,6 +29,20 @@ export default React.memo(() => {
         <p>{`Real Name: ${user.real_name}`}</p>
         <p>{`Student Id: ${user.student_id}`}</p>
         <p>{`Class: ${user.class}`}</p>
+      </div>
+    )}
+    {boards.map(board => 
+      <div>
+        <p>{`Remove: ${board.remote}`}</p>
+        <p>{`Software Version: ${board.software_version}`}</p>
+        <p>{`Hardware Version: ${board.hardware_version}`}</p>
+      </div>
+    )}
+    {jobs.map(job => 
+      <div>
+        <p>{`Submitter: ${job.submitter}`}</p>
+        <p>{`Src: ${job.src_url}`}</p>
+        <p>{`Dst: ${job.dst_url}`}</p>
       </div>
     )}
   </main>;
