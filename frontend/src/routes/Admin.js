6a8700e4209f2b2ecd33c6151575084468cccbfa
Jiajie Chen <noc@jiegec.ac.cn>
2020-02-18 19:22:05 +0800

    Add paging for tasks

--- frontend/src/routes/Admin.js
+++ frontend/src/routes/Admin.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import { get } from '../util';
@@ -9,6 +9,8 @@ import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
+import TableFooter from '@material-ui/core/TableFooter';
+import TablePagination from '@material-ui/core/TablePagination';
 import Paper from '@material-ui/core/Paper';
 import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
@@ -17,17 +19,36 @@ export default React.memo(() => {
   const [users, setUsers] = useState([]);
   const [boards, setBoards] = useState([]);
   const [jobs, setJobs] = useState([]);
-  const history = useHistory();
-  const dispatch = useDispatch();
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
 
   const doRefresh = useCallback(async () => {
     let result = await get('/api/user/list?offset=0&limit=-1');
     setUsers(result.users);
     result = await get('/api/board/list');
     setBoards(result);
-    result = await get('/api/task/list?offset=0&limit=-1');
+    let offset = page * rowsPerPage;
+    if (rowsPerPage < 0) {
+      // all
+      offset = 0;
+    }
+    result = await get(`/api/task/list?offset=${offset}&limit=${rowsPerPage}`);
     setJobs(result.jobs);
-  }, []);
+  }, [page, rowsPerPage]);
+
+  const handleChangePage = useCallback((e, v) => {
+    setPage(v);
+    doRefresh();
+  }, [doRefresh]);
+
+  const handleChangeRowsPerPage = useCallback(e => {
+    setRowsPerPage(parseInt(e.target.value, 10));
+    doRefresh();
+  }, [doRefresh]);
+
+  useEffect(() => {
+    doRefresh();
+  }, [doRefresh]);
 
   return <main>
     <Container>
@@ -96,6 +117,23 @@ export default React.memo(() => {
             ))}
           </TableBody>
         </Table>
+        <TableFooter>
+          <TableRow>
+            <TablePagination
+              rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
+              colSpan={3}
+              count={jobs.length}
+              rowsPerPage={rowsPerPage}
+              page={page}
+              SelectProps={{
+                inputProps: { 'aria-label': 'rows per page' },
+                native: true,
+              }}
+              onChangePage={handleChangePage}
+              onChangeRowsPerPage={handleChangeRowsPerPage}
+            />
+          </TableRow>
+        </TableFooter>
       </TableContainer>
     </Container>
   </main>;
