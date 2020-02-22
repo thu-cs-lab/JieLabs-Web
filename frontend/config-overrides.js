722150e2351b4f10deaeb1eb99c6884f63c34d7c
Liu Xiaoyi <circuitcoder0@gmail.com>
2020-02-22 15:21:09 +0800

    Remove redundant workers for monaco

--- frontend/config-overrides.js
+++ frontend/config-overrides.js
@@ -7,6 +7,8 @@ const {
 const MonacoPlugin = require('monaco-editor-webpack-plugin');
 
 module.exports = override(
-  addWebpackPlugin(new MonacoPlugin()),
+  addWebpackPlugin(new MonacoPlugin({
+    languages: [],
+  })),
   addWebpackModuleRule({ test: /\.wasm$/, type: 'webassembly/experimental' }),
 );
