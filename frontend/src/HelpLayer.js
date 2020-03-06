92c03a44c9110d67f0f8456235c3aeaa92252ab6
Liu Xiaoyi <circuitcoder0@gmail.com>
2020-03-07 07:07:33 +0800

    Enable reset button

--- frontend/src/HelpLayer.js
+++ frontend/src/HelpLayer.js
@@ -8,7 +8,6 @@ import { unstepHelp, stepHelp, endHelp } from './store/actions';
 
 const STEPS = [
   {
-    backdrop: true,
     done: () => true,
     renderer: () => (
       <div className="help-welcome">
@@ -54,12 +53,21 @@ function useStep(step) {
     if(step === STEPS.length - 1) return () => dispatch(endHelp());
     return () => dispatch(stepHelp());
   }, [step]);
+  const reset = useMemo(() => {
+    if(!config?.reset) return null;
+    return () => config.reset(state, dispatch);
+  }, [step]);
+
+  // Reset on step changes
+  useEffect(() => {
+    if(reset) reset();
+  }, [reset]);
 
   return {
     prev,
     next,
 
-    backdrop: config ? config.backdrop : false,
+    reset,
     renderer: config ? config.renderer : () => {},
   };
 }
@@ -68,7 +76,7 @@ export default React.memo(() => {
   const dispatch = useDispatch();
   const step = useSelector(state => state.help);
 
-  const { prev, next, renderer, backdrop } = useStep(step);
+  const { prev, next, reset, renderer } = useStep(step);
 
   const open = step !== null;
 
@@ -85,14 +93,14 @@ export default React.memo(() => {
   }, [hidden]);
 
   return <div className={cn("help", { 'help-open': open })}>
-    <div className={cn("help-backdrop", { 'help-backdrop-shown': open && backdrop && !hidden })}></div>
+    <div className={cn("help-backdrop", { 'help-backdrop-shown': open && !hidden })}></div>
 
     <div className="help-controller-cont">
       <div className="help-controller">
         <Icon className={cn("help-controller-action", { 'help-controller-action-disabled': !prev})} onClick={prev}>skip_previous</Icon>
         <Icon className={cn("help-controller-action", { 'help-controller-action-disabled': !next })} onClick={next}>skip_next</Icon>
         <Icon className="help-controller-action" onClick={toggleHidden}>{ hidden ? 'tab' : 'tab_unselected' }</Icon>
-        <Icon className="help-controller-action">settings_backup_restore</Icon>
+        <Icon className={cn("help-controller-action", { 'help-controller-action-disabled': !reset })} onClick={reset}>settings_backup_restore</Icon>
         <Icon className="help-controller-action" onClick={exit}>stop</Icon>
       </div>
     </div>
