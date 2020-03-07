30dc38116214ad07a3d5a07e6d6e08637fa8666b
Jiajie Chen <noc@jiegec.ac.cn>
2020-03-07 12:07:30 +0800

    Fix typo: onChange -> onUpdate

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -41,7 +41,7 @@ ReactDOM.render(<Render />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.register({
-  onChange() {
+  onUpdate() {
     store.dispatch(setUpdateAvailable());
   }
 });
