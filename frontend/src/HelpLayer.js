4d40fa88ae953755668e20155b8b95bb630ccf60
Liu Xiaoyi <circuitcoder0@gmail.com>
2020-03-07 11:32:05 +0800

    Finished interactive tutor. Closes #11

--- frontend/src/HelpLayer.js
+++ frontend/src/HelpLayer.js
@@ -5,7 +5,9 @@ import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 import Icon from './comps/Icon';
 import Tooltip from './comps/Tooltip';
-import { unstepHelp, stepHelp, endHelp, updateCode, assignTop } from './store/actions';
+import { BOARD_STATUS, unstepHelp, stepHelp, endHelp, updateCode, updateTop, deleteBlock, pushBlock } from './store/actions';
+
+import { DEFAULT_FIELD } from './config';
 
 import src from './assets/tutorial.vhdl'; // eslint-disable-line
 
@@ -143,6 +145,7 @@ const STEPS = [
       );
     }
   }, {
+    region: 'editor-only',
     done: state => {
       const rst = state.constraints.signals.get('rst');
       const clk = state.constraints.signals.get('clk');
@@ -150,7 +153,7 @@ const STEPS = [
       return rst === 1 && clk === 37 && toggle === 0;
     },
     reset: (state, dispatch) => {
-      dispatch(assignTop(null));
+      dispatch(updateTop(null));
     },
     renderer: (state) => {
       return (
@@ -172,6 +175,143 @@ const STEPS = [
         </div>
       );
     }
+  }, {
+    done: (state) => {
+      const latestStatus = state.builds.list.get(0)?.status;
+      return latestStatus === 'Compilation Success';
+    },
+    renderer: () => (
+      <div className="help-layout">
+        <div className="help-layout-toolbar">
+          <div className="help-layout-row">
+            <Icon>build</Icon>
+            <div className="help-layout-desc">点这里<del>发给杰哥优化</del>提交编译</div>
+          </div>
+        </div>
+
+        <div className="help-layout-build">
+          编译状态
+        </div>
+
+        <div className="help-box" style={{
+          position: 'absolute',
+          top: 60*2 + 100,
+          left: '50vw',
+          transform: 'translateX(-50%)',
+        }}>
+          <strong>提交编译！</strong>
+          <p>当顶级实体及其所有的信号都被映射之后，就可以提交编译了！</p>
+          <p>点击中央工具栏中的编译按钮提交之后，编译状态会更新为新提交的状态。等待提交状态从代表进行中的蓝色方块变成代表成功的绿色对钩之后，就可以进入下一步了。</p>
+          <hr />
+          <p>在进入下一步前，可以试试将鼠标放置在编译状态上，JieLabs 将会展开一个列表，显示你的所有过往提交。在这个列表中你也可以上传过往提交生成的 Bitstream，以及查看编译信息和代码。</p>
+        </div>
+      </div>
+    )
+  }, {
+    region: 'sandbox-only',
+    done: state => {
+      if(state.board.status !== BOARD_STATUS.CONNECTED) return false;
+      if(state.activeBuild === null) return false;
+      return true;
+    },
+    renderer: () => {
+      return (
+        <div className="help-box" style={{
+          width: 'calc(50vw - 80px)',
+          maxWidth: 'unset',
+          position: 'fixed',
+          top: 40,
+          right: 40,
+        }}>
+          <strong>连接 / 烧写 FPGA</strong>
+          <p>Bitstream 已经就绪，现在可以将注意放在沙盒上了。</p>
+          <p>作为编程工作的收尾，我们可以通过以下流程将 Bitstream 烧写到 FPGA 上：</p>
+          <ul>
+            <li>如果 FPGA 模块显示还没有分配 FPGA，可以通过点击 FPGA 模块进行申请。</li>
+            <li>分配完成后，位于编译按钮下方的上传 Bitstream 按钮激活，点击即可开始烧写。</li>
+          </ul>
+          <p>完成烧写后可以进行下一步。</p>
+        </div>
+      );
+    },
+  }, {
+    region: 'sandbox-only',
+    reset: (state, dispatch) => {
+      const size = state.field.size;
+      for(let i = 0; i < size; ++i) dispatch(deleteBlock(0));
+      for(const spec of DEFAULT_FIELD) dispatch(pushBlock(spec));
+    },
+    done: state => {
+      const field = state.field;
+      const hasFPGA = !!field.find(e => e.type === 'FPGA');
+      const hasClock = !!field.find(e => e.type === 'Clock');
+      const hasSwitch4 = !!field.find(e => e.type === 'Switch4');
+
+      return field.size === 3 && hasFPGA && hasClock && hasSwitch4;
+    },
+    renderer: () => {
+      return (
+        <div className="help-box" style={{
+          width: 'calc(50vw - 80px)',
+          maxWidth: 'unset',
+          position: 'fixed',
+          top: 40,
+          right: 40,
+        }}>
+          <strong>亮，都可以亮</strong>
+          <p>现在 FPGA 内已经烧进我们编写的逻辑了。接下来需要将 FPGA 的引脚和外部模块连接，以便控制、观察 FPGA 的输入输出。</p>
+          <p>沙盒内预先安排了一些模块，从左到右，从上到下，分别是</p>
+          <ul>
+            <li>FPGA 模块</li>
+            <li>带译码数码管</li>
+            <li>无译码数码管</li>
+            <li>LED+开关，上方为 LED，下方为开关</li>
+            <li>时钟模块，上方为时钟发生器引脚，下方为两个按钮，可以作为 Reset 或者手动时钟。</li>
+          </ul>
+
+          <p>右键沙盒内的任意位置可以添加新模块，既有的模块的删除按钮也可以通过将鼠标放置于模块上展现。请将两个数码管模块删除，我们暂时用不到它们。</p>
+        </div>
+      );
+    },
+  }, {
+    region: 'sandbox-only',
+    done: () => true,
+    renderer: () => {
+      return (
+        <div className="help-box" style={{
+          width: 'calc(50vw - 80px)',
+          maxWidth: 'unset',
+          position: 'fixed',
+          top: 40,
+          right: 40,
+        }}>
+          <strong>亮，都可以亮 (续)</strong>
+          <p>点击接线柱可以开始连线，在连线的过程中，被连接的接线柱会被显示为蓝色，鼠标经过其他接线柱或者其他线时，将会将其高亮为黄色。点击被高亮为黄色的接线柱或线，可以尝试连线。如果没有发生以下错误，那么连线将会成功：</p>
+          <ul>
+            <li>如果时钟发生器连接至除了 FPGA 37 号引脚以外的地方，将会失败</li>
+            <li>如果一个接线柱已经通过网络和即将连线的线或者接线柱相连。</li>
+          </ul>
+          <p>点击蓝色接线柱或者沙盒中任意空地可以取消连线。尝试将其中一个 LED 连接至 FPGA 0 号引脚，4M 时钟发生器至 37 号引脚，以及一个按钮至 1 号引脚。点击按钮，你应该能看到 LED 开始以 1s 为间隔开始闪烁。</p>
+          <p>我们没办法检查这一任务的完成与否，但是这就是指南的最后一步了！</p>
+          <p>通过左下角的沙箱工具栏或者 Ctrl-F 快捷键，你可以修改连线颜色，以及在模块和连线层之间切换。在连线层中你可以删除连线，或者断开特定的接线柱。尝试连接其他频率的时钟，或者将输出同时连接到多个 LED 上。当你认为自己对沙箱机制的理解已经够用了的话，可以前往下一步。</p>
+          <hr />
+          <p>你可能注意到 FPGA 的 37 号引脚和时钟发生器的接线柱颜色和其他接线柱不同。蓝色的接线柱是标记这里的信号和时钟信号有关。对于 FPGA 而言，只有 37 号引脚可以接入高频时钟，它和时钟发生器的区别在于，37 号引脚可以连接手动时钟，但是时钟发生器不能连接 LED、数码管等普通的显示器件。</p>
+        </div>
+      );
+    },
+  }, {
+    done: () => true,
+    renderer: () => {
+      return (
+      <div className="help-welcome">
+        <div className="help-box">
+          <div className="brand"><strong>Jie</strong>Labs</div>
+          <p>指南到此结束了！在中央工具栏中有帮助按钮，你可以通过它找到快捷键 Cheatsheet，还能重新启动这一指南。</p>
+          <p>祝你在数字电路的海洋中能够一帆风顺，不会翻船！</p>
+        </div>
+      </div>
+    );
+    },
   }
 ];
 
@@ -237,6 +377,7 @@ export default React.memo(() => {
     <div className={cn("help-backdrop", {
       'help-backdrop-shown': open && !hidden,
       'help-backdrop-editor-only': region === 'editor-only',
+      'help-backdrop-sandbox-only': region === 'sandbox-only',
     })}>
       <div className="help-backdrop-stripe"></div>
     </div>
