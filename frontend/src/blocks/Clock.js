5f0a5ba6e26dc41d23183227b40d1c806ab573bb
Liu Xiaoyi <circuitcoder0@gmail.com>
2020-03-09 19:14:33 +0800

    Reset FPGA timeout by interacting with sandbox inputs

--- frontend/src/blocks/Clock.js
+++ frontend/src/blocks/Clock.js
@@ -1,13 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import cn from 'classnames';
 
 import { Connector, SIGNAL, MODE } from './index.js';
+
+import { TimeoutContext } from '../App';
 
 const CLOCK_FREQUENCY = [16, 8, 4, 2, 1];
 
 export default React.memo(({ id, ...rest }) => {
   const [manualClock, setManualClock] = useState(SIGNAL.L);
   const [manualReset, setManualReset] = useState(SIGNAL.L);
+
+  const timeoutCtx = useContext(TimeoutContext);
+
+  function getHandler(setter, signal) {
+    return e => {
+      e.stopPropagation();
+      setter(signal);
+      timeoutCtx.reset();
+    }
+  }
 
   return <div className="block clock" {...rest}>
     <div className="clock-top">
@@ -30,16 +42,16 @@ export default React.memo(({ id, ...rest }) => {
         <Connector output={manualClock} id={`${id}-clk`}></Connector>
         <div
           className={cn("press-btn", { pusheen: manualClock === SIGNAL.H })}
-          onMouseDown={e => { e.stopPropagation(); setManualClock(SIGNAL.H); }}
-          onMouseUp={e => { e.stopPropagation(); setManualClock(SIGNAL.L); }}
+          onMouseDown={getHandler(setManualClock, SIGNAL.H)}
+          onMouseUp={getHandler(setManualClock, SIGNAL.L)}
         ></div>
       </div>
       <div className="clock-box">
         <Connector output={manualReset} id={`${id}-rst`}></Connector>
         <div
           className={cn("press-btn", { pusheen: manualReset === SIGNAL.H })}
-          onMouseDown={e => { e.stopPropagation(); setManualReset(SIGNAL.H); }}
-          onMouseUp={e => { e.stopPropagation(); setManualReset(SIGNAL.L); }}
+          onMouseDown={getHandler(setManualReset, SIGNAL.H)}
+          onMouseUp={getHandler(setManualReset, SIGNAL.L)}
         ></div>
       </div>
     </div>
--- frontend/src/blocks/Switch4.js
+++ frontend/src/blocks/Switch4.js
@@ -1,12 +1,16 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useContext } from 'react';
 import cn from 'classnames';
 import { List } from 'immutable';
 
 import { Connector, SIGNAL } from './index.js';
 
+import { TimeoutContext } from '../App';
+
 export default React.memo(({ id, ...rest }) => {
   const [leds, setLeds] = useState(List(Array(4).fill(SIGNAL.X)));
   const [switches, setSwitches] = useState(List(Array(4).fill(SIGNAL.L)));
+
+  const timeoutCtx = useContext(TimeoutContext);
 
   const currentLeds = useRef(leds);
 
@@ -30,7 +34,10 @@ export default React.memo(({ id, ...rest }) => {
         switches.map((s, idx) => <div key={idx}>
           <div
             className={cn("switch", { pusheen: s === SIGNAL.H })}
-            onClick={() => setSwitches(switches.set(idx, s === SIGNAL.L ? SIGNAL.H : SIGNAL.L))}
+            onClick={() => {
+              setSwitches(switches.set(idx, s === SIGNAL.L ? SIGNAL.H : SIGNAL.L));
+              timeoutCtx.reset();
+            }}
             onMouseDown={e => e.stopPropagation()}
           ></div>
           <Connector id={`${id}-switch-${idx}`} output={s}></Connector>
