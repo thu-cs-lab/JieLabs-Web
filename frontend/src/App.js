a7dbe3e227dbde9f5dec10ba40d298e2bb4cb0e1
Jiajie Chen <noc@jiegec.ac.cn>
2020-03-19 12:47:52 +0800

    Add experimental to verilog (again)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -594,7 +594,7 @@ export default React.memo(() => {
         <div className="languages">
           <div id="verilog" className={cn('language', { 'language-selected': lang === 'verilog' })} onClick={() => setLanguage('verilog')}>
             <div className="language-ind"><Icon>done</Icon></div>
-            <div className="language-name">Verilog</div>
+            <div className="language-name">Verilog (experimental)</div>
           </div>
 
           <div id="vhdl" className={cn('language', { 'language-selected': lang === 'vhdl' })} onClick={() => setLanguage('vhdl')}>
