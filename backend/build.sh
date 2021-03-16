df6c0cb049830c16f52aa6a4c9f2143033ed0589
jiegec <cslab@gateway1.s.cslab.moe>
2021-03-16 14:55:05 +0800

    Better cache with builder

--- backend/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-docker build -t thu-cs-lab/jielabs-backend .
-docker tag thu-cs-lab/jielabs-backend:latest $REGISTRY/thu-cs-lab/jielabs-backend:latest
-docker push $REGISTRY/thu-cs-lab/jielabs-backend:latest
---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# use with: DOCKER_ARGS="--build-arg https_proxy=proxy" REGISTRY=host:port ./build.sh 
+set -e
+
+cd backend
+docker pull $REGISTRY/thu-cs-lab/jielabs-backend:builder || true
+docker build --target builder --cache-from $REGISTRY/thu-cs-lab/jielabs-backend:builder $DOCKER_ARGS -t thu-cs-lab/jielabs-backend:builder .
+docker tag thu-cs-lab/jielabs-backend:builder $REGISTRY/thu-cs-lab/jielabs-backend:builder
+docker push $REGISTRY/thu-cs-lab/jielabs-backend:builder
+docker pull $REGISTRY/thu-cs-lab/jielabs-backend:latest || true
+docker build --cache-from $REGISTRY/thu-cs-lab/jielabs-backend:builder --cache-from $REGISTRY/thu-cs-lab/jielabs-backend:latest $DOCKER_ARGS -t thu-cs-lab/jielabs-backend:latest .
+docker tag thu-cs-lab/jielabs-backend:latest $REGISTRY/thu-cs-lab/jielabs-backend:latest
+docker push $REGISTRY/thu-cs-lab/jielabs-backend:latest
+cd ..
+
