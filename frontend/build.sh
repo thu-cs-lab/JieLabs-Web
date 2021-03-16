f1cc0b7e011ec7347d3e7aef973b64fe6709dc4e
Liu Xiaoyi <circuitcoder0@gmail.com>
2021-03-17 02:24:27 +0800

    Build frontend/backend in parallel. Also, auto deploy frontend

--- frontend/build.sh
+++ frontend/build.sh
@@ -1,20 +1,6 @@
 #!/bin/sh
-# use with: DOCKER_ARGS="--build-arg https_proxy=proxy" REGISTRY=host:port ./build.sh 
 set -e
 
-cd backend
-docker pull $REGISTRY/thu-cs-lab/jielabs-backend:builder || true
-docker build --target builder --cache-from $REGISTRY/thu-cs-lab/jielabs-backend:builder $DOCKER_ARGS -t thu-cs-lab/jielabs-backend:builder .
-docker tag thu-cs-lab/jielabs-backend:builder $REGISTRY/thu-cs-lab/jielabs-backend:builder
-docker push $REGISTRY/thu-cs-lab/jielabs-backend:builder
-docker pull $REGISTRY/thu-cs-lab/jielabs-backend:latest || true
-docker build --cache-from $REGISTRY/thu-cs-lab/jielabs-backend:builder --cache-from $REGISTRY/thu-cs-lab/jielabs-backend:latest $DOCKER_ARGS -t thu-cs-lab/jielabs-backend:latest .
-docker tag thu-cs-lab/jielabs-backend:latest $REGISTRY/thu-cs-lab/jielabs-backend:latest
-docker push $REGISTRY/thu-cs-lab/jielabs-backend:latest
-cd ..
-
-
-cd frontend
 docker pull $REGISTRY/thu-cs-lab/jielabs-frontend:wasm || true
 docker build --target wasm --cache-from $REGISTRY/thu-cs-lab/jielabs-frontend:wasm $DOCKER_ARGS -t thu-cs-lab/jielabs-frontend:wasm .
 docker tag thu-cs-lab/jielabs-frontend:wasm $REGISTRY/thu-cs-lab/jielabs-frontend:wasm
@@ -38,4 +24,3 @@ docker build \
   $DOCKER_ARGS -t thu-cs-lab/jielabs-frontend:latest .
 docker tag thu-cs-lab/jielabs-frontend:latest $REGISTRY/thu-cs-lab/jielabs-frontend:latest
 docker push $REGISTRY/thu-cs-lab/jielabs-frontend:latest
-cd ..
