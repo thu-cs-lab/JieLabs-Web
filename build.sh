eec11b3c2fa7f847e8f34e41d18ef8e19b3fa5f8
Liu Xiaoyi <circuitcoder0@gmail.com>
2021-03-17 02:10:15 +0800

    Apply commit sha in the final build stage in frontend

--- build.sh
+++ build.sh
@@ -24,7 +24,8 @@ docker pull $REGISTRY/thu-cs-lab/jielabs-frontend:builder || true
 docker build --target builder \
   --cache-from $REGISTRY/thu-cs-lab/jielabs-frontend:wasm \
   --cache-from $REGISTRY/thu-cs-lab/jielabs-frontend:builder \
-  $DOCKER_ARGS --build-arg commit_sha=$CI_COMMIT_SHA -t thu-cs-lab/jielabs-frontend:builder .
+  --build-arg commit_sha=$CI_COMMIT_SHA \
+  $DOCKER_ARGS -t thu-cs-lab/jielabs-frontend:builder .
 docker tag thu-cs-lab/jielabs-frontend:builder $REGISTRY/thu-cs-lab/jielabs-frontend:builder
 docker push $REGISTRY/thu-cs-lab/jielabs-frontend:builder
 
@@ -33,6 +34,7 @@ docker build \
   --cache-from $REGISTRY/thu-cs-lab/jielabs-frontend:wasm \
   --cache-from $REGISTRY/thu-cs-lab/jielabs-frontend:builder \
   --cache-from $REGISTRY/thu-cs-lab/jielabs-frontend:latest \
+  --build-arg commit_sha=$CI_COMMIT_SHA \
   $DOCKER_ARGS -t thu-cs-lab/jielabs-frontend:latest .
 docker tag thu-cs-lab/jielabs-frontend:latest $REGISTRY/thu-cs-lab/jielabs-frontend:latest
 docker push $REGISTRY/thu-cs-lab/jielabs-frontend:latest
