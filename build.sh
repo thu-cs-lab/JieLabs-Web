35d3653d8a5d53456035af47a4bb36873ec19a5d
Liu Xiaoyi <circuitcoder0@gmail.com>
2021-03-17 00:27:42 +0800

    Explicitly builds the wasm container in frontend

--- build.sh
+++ build.sh
@@ -15,10 +15,16 @@ cd ..
 
 
 cd frontend
+docker pull $REGISTRY/thu-cs-lab/jielabs-frontend:wasm || true
+docker build --target wasm --cache-from $REGISTRY/thu-cs-lab/jielabs-frontend:wasm $DOCKER_ARGS -t thu-cs-lab/jielabs-frontend:wasm .
+docker tag thu-cs-lab/jielabs-frontend:wasm $REGISTRY/thu-cs-lab/jielabs-frontend:wasm
+docker push $REGISTRY/thu-cs-lab/jielabs-frontend:wasm
+
 docker pull $REGISTRY/thu-cs-lab/jielabs-frontend:builder || true
 docker build --target builder --cache-from $REGISTRY/thu-cs-lab/jielabs-frontend:builder $DOCKER_ARGS -t thu-cs-lab/jielabs-frontend:builder .
 docker tag thu-cs-lab/jielabs-frontend:builder $REGISTRY/thu-cs-lab/jielabs-frontend:builder
 docker push $REGISTRY/thu-cs-lab/jielabs-frontend:builder
+
 docker pull $REGISTRY/thu-cs-lab/jielabs-frontend:latest || true
 docker build --cache-from $REGISTRY/thu-cs-lab/jielabs-frontend:builder --cache-from $REGISTRY/thu-cs-lab/jielabs-frontend:latest $DOCKER_ARGS -t thu-cs-lab/jielabs-frontend:latest .
 docker tag thu-cs-lab/jielabs-frontend:latest $REGISTRY/thu-cs-lab/jielabs-frontend:latest
