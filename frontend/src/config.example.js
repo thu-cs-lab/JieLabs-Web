5c53c698ae46c9677decc6b72ecb05886312ee44
Jiajie Chen <c@jia.je>
2021-03-24 00:39:16 +0800

    Setup mime type for wasm

--- frontend/src/config.example.js
+++ frontend/src/config.example.js
@@ -1,5 +1,3 @@
-const BACKEND_STEM = process.env.REACT_APP_SENTRY ?? '//localhost:8080'
-
 export const BACKEND = process.env.REACT_APP_BACKEND || 'http://localhost:8080';
 export const WS_BACKEND = BACKEND.replace(/^http/, 'ws');
 export const HARD_LOGOUT = false;
